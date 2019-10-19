6f2cb4602be283b2341fa9e34a7f64d9777201a6
Kevin <stradtkt22@gmail.com>
2019-10-19 11:48:57 -0400

    Implemented localstorage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -21,13 +21,21 @@ export class LogService {
     this.logs = [];
   }
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+    return of(this.logs.sort((a, b) => {
+      return b.date = a.date;
+    }));
   }
   setFormLog(log: Log) {
     this.logSource.next(log);
   }
   addLog(log: Log) {
     this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
   updateLog(log: Log) {
     this.logs.forEach((current, index) => {
@@ -36,6 +44,7 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
   deleteLog(log: Log) {
     this.logs.forEach((current, index) => {
@@ -43,6 +52,7 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
   clearState() {
     this.stateSource.next(true);
